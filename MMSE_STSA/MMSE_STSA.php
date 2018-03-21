be4ac052aff58b3e7710bcfabc787d1d92ace456
Ed Baker <edwbaker@gmail.com>
2018-03-21 15:29:21 +0000

    Update MMSE_STSA.php

--- MMSE_STSA/MMSE_STSA.php
+++ MMSE_STSA/MMSE_STSA.php
@@ -29,6 +29,31 @@ function MMSE_STSA_prepare() {
 function MMSE_STSA_transcode($data) {
   global $system;
   $return = array();
+  if (!in_array($data["id"].".MMSE_STSA.wav", $system["analyses"]["wav"])) {
+    core_log("info", "MMSE_STSA", "File ".$data["id"]." needs MMSE_STSA version.");
+    $file = core_download("wav/".$data["id"].".wav");
+    if ($file = NULL) {
+      core_log("warning", "MMSE_STSA", "File could not be downloaded.");
+      return($return);
+    }
+    $return[$data["id"]] = array(
+      "file name" => $data["id"].".wav",
+      "local path" => "scratch/wav/",
+      "save path" => NULL
+    );
+    exec("java -Xmx20148m -classpath modules/traits-MMSE_STSA/MMSE_STSA/AudioProcessor DenoisingExample scratch/wav/".$data["id"].".wav", $output, $return_value);
+    exec("mv scratch/wav/".$data["id"]."_enhanced.wav scratch/wav/".$data["id"].".MMSE_STSA.wav");
+    if ($return_value == 0) {
+      $return[$data["id"]."1k_MMSE"] = array(
+        "file name" => $data["id"].".MMSE_STSA.wav",
+        "local path" => "scratch/wav/",
+        "save path" => "wav/"
+      );
+    } else {
+      core_log("warning", "bioacoustica", "Could not download file for BioAcosutica recording ".$data["id"].".");
+    }
+  }
+  
   if (!in_array($data["id"].".1kHz-highpass.MMSE_STSA.wav", $system["analyses"]["wav"])) {
     core_log("info", "MMSE_STSA", "File ".$data["id"]." needs MMSE_STSA version.");
     $file = core_download("wav/".$data["id"].".1kHz-highpass.wav");
